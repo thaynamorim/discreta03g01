1deabed819aaf82dfedc9802354ccda9980f0cbe
Lucas Canejo Jurema <lucasbrgol2010@hotmail.com>
2015-09-11 13:59:58 -0300

    adicionado nivel de debug 2 + algumas saidas

--- saida.c
+++ saida.c
@@ -7,7 +7,7 @@ if(debug==0)
     for(i=0;i<VMAX;i++)
         for(j=0;j<VMAX;j++)
             if (tl[i][j]==0)
-                print("Erro. Nenhuma transicao habilitada");
+                printf("Erro. Nenhuma transicao habilitada");
 }
 
 if(debug==1)
@@ -17,4 +17,14 @@ if(debug==1)
             if (tl[i][j]==0)
                 printf("Erro. Os tokens dos lugares nao sao o suficiente para habilitar todas as transicoes");
 }
+
+if(debug==0)
+    printf("O numero de tokens no final foi igual a %2d", &l[i]);
+
+if(debug==2)
+{
+    for(i=0,i<VMAX;i++)
+        printf("O numero de tokens no final da transicao %2d foi igual a %2d \n", &lt[i], &l[i]);
+}
+
 
