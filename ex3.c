cb89b0a8adfce37fc9b63c89c5b584b5e17e002b
Lucas Canejo Jurema <lucasbrgol2010@hotmail.com>
2015-09-13 18:05:41 -0300

    Cabecalho doxygen

--- ex3.c
+++ ex3.c
@@ -1,3 +1,52 @@
+/* ************************************************************************ *
+ *    ex3.c, v1.0                                                           *
+ *    Programa feito para simular uma rede de petri dado um arquivo de      *
+ *    texto como entrada                                                    *
+ *                                                                          *
+ *    Copyright (C) 2015                                                    *
+ *    Hugo Albuquerque de Fonseca                                           *
+ *    Lucas Canejo Jurema                                                   *
+ *    Mateus Lenier Rezende                                                 *
+ *    Paulo Vitor Alves Patriota                                            *
+ *    Thayna Maria Morim de Barros Barreto                                  *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *    To contact the author, please write to:                               *
+ *                                                                          *
+ *    Hugo Albuquerque de Fonseca <hugoalbuquerque0@gmail.com>              *
+ *    Phone: 55 (819) 9875-6561                                             *
+ *                                                                          *
+ *    Lucas Canejo Jurema <lucasbrgol2010@hotmail.com>                      *
+ *    Phone: 55 (819) 9547-1048                                             *
+ *                                                                          *    
+ *    Mateus Lenier Rezende <mateuslenier@gmail.com>                        *
+ *    Phone: 55 (819) 9891-2596                                             *
+ *                                                                          *
+ *    Paulo Vitor Alves Patriota <paulovitorpatriota@gmail.com>             *
+ *    Phone: 55 (819) 9966-1154                                             *
+ *                                                                          *
+ *    Thayna Maria Morim de Barros Barreto <tmorimbarreto@gmail.com>        *
+ *    Phone: 55 (819) 9746-8034                                             *
+ *                                                                          *
+ *                                                                          *       
+ ************************************************************************** *
+ *                                                
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
